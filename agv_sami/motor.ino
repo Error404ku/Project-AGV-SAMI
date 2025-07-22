d8fa718a27c4c9b525066a2a0d6a8795b14b43a5
mhusainh <husainhasbulah@gmail.com>
2025-07-22 12:11:30 +0700

    nice

--- agv_sami/motor.ino
+++ agv_sami/motor.ino
@@ -102,19 +102,19 @@ void pwmMotor(int motor1, int motor2)
     if (motor1 > 0) {
         // Motor kanan maju
         digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
+        // digitalWrite(IN2, LOW);
         ledcWrite(channelKanan, motor1);
     }
     else if (motor1 < 0) {
         // Motor kanan mundur
         digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
+        // digitalWrite(IN2, HIGH);
         ledcWrite(channelKanan, abs(motor1));
     }
     else {
         // Motor kanan stop
         digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
+        // digitalWrite(IN2, LOW);
         ledcWrite(channelKanan, 0);
     }
     
@@ -122,19 +122,19 @@ void pwmMotor(int motor1, int motor2)
     if (motor2 > 0) {
         // Motor kiri maju
         digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
+        // digitalWrite(IN4, LOW);
         ledcWrite(channelKiri, motor2);
     }   
     else if (motor2 < 0) {
         // Motor kiri mundur
         digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
+        // digitalWrite(IN4, HIGH);
         ledcWrite(channelKiri, abs(motor2));
     }
     else {   
         // Motor kiri stop
         digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
+        // digitalWrite(IN4, LOW);
         ledcWrite(channelKiri, 0);
     }   
 }
--- agv_sami/setup.ino
+++ agv_sami/setup.ino
@@ -8,11 +8,11 @@ void setupMotor() {
   pinMode(ENB, OUTPUT);
 
   // Setup PWM for enable pins (ENA dan ENB)
-  ledcSetup(channelKanan, pwmFrequency, pwmResolution);
-  ledcAttachPin(ENA, channelKanan);
+  // ledcSetup(channelKanan, pwmFrequency, pwmResolution);
+  ledcAttachChannel(ENA, pwmFrequency, pwmResolution, channelKanan);
 
-  ledcSetup(channelKiri, pwmFrequency, pwmResolution);
-  ledcAttachPin(ENB, channelKiri);
+  // ledcSetup(channelKiri, pwmFrequency, pwmResolution);
+  ledcAttachChannel(ENB, pwmFrequency, pwmResolution, channelKiri);
   
   // Matikan motor saat startup
   digitalWrite(IN1, LOW);
